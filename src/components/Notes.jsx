245f21d8803d30355b20b1e89f9e43809810c1eb
Vikashbaabhu <75823844+Vikashbaabhu@users.noreply.github.com>
2022-07-19 23:05:16 +0530

    Add files via upload

---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is Note Taking App</h1>
+      <p>
+        This App is Used for taking notes and store all notes and important
+        information digitally, usually in a cloud-based storage system.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
